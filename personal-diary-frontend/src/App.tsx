aecc9fde1ead71de98f93d0ff1aa6289c4b31240
ssamal2801 <sinu2290@gmail.com>
2023-07-11 06:53:22 -0400

    frontend project created

---
+++ personal-diary-frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { styled } from 'styled-components';
+import './App.css'
+
+function App() {
+
+  const AppContainer = styled.div`
+    min-height: 100vh;`;
+
+  return (
+    <AppContainer>
+      this is the app
+    </AppContainer>
+  )
+}
+
+export default App
